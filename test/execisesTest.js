31e378d8f5a61944656fce507d5a86d603fe1c1b
Eduardo Romo Gomez <eduardo.romo.gomez@ing.com>
2018-04-20 14:17:47 +0200

    improve tests

--- test/execisesTest.js
+++ test/execisesTest.js
@@ -8,7 +8,7 @@ const expect = chai.expect;
 describe('exercises (all the data passed to functions comes from data.js file, unless explicitly stated on the test)', () => {
 
     describe('basics', () => {
-        // suggestion: use currify and map
+        // suggestion: use map
         xit('return a new array greeting all names', () => {
             expect(greetAll(names)).to.deep.equal(['hi juan', 'hi ivan', 'hi jose', 'hi sebas', 'hi miguel', 'hi ricardo', 'hi edu']);
         });
