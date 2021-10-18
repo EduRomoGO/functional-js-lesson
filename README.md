18660fe7f4ee42fe517e9d22ea883a5f00913b1a
EduRomoGO <eduromogo@outlook.es>
2021-10-18 17:27:02 +0200

    docs

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Run `npm i`  
 Run `npm test` or `npm run test`
+
+## Exercises
+
+Your job is to go to `exercisesTest.js` and follow test suggestions to make the test pass.
--- curry.js
+++ curry.js
@@ -1,11 +1,14 @@
-const objects = [{ name: 'aa1', surname: 'bb1', id: 1 }, { name: 'aa2', surname: 'bb2', id: 2 }, { name: 'aa3', surname: 'bb3', id: 3 }];
-funtion  ppp(o, property) {
-    return o[property];
+const objects = [
+  { name: "aa1", surname: "bb1", id: 1 },
+  { name: "aa2", surname: "bb2", id: 2 },
+  { name: "aa3", surname: "bb3", id: 3 },
+];
+function ppp(o, property) {
+  return o[property];
 }
-objects.map((o,) => ppp());
-objects.map(o => o.name);
-
+objects.map((o) => ppp());
+objects.map((o) => o.name);
 
 const get = curry((property, object) => object[property]);
-objects.map(get('id')) //= [1, 2, 3]
-objects.map(get('name')) //= [1, 2, 3]
+objects.map(get("id")); //= [1, 2, 3]
+objects.map(get("name")); //= [1, 2, 3]
